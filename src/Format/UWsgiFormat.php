5982c4df0e1c7f21ac0f13af4f383e5312f320b4
Justinas Urbanavicius <justinasu@gmail.com>
2017-03-13 21:52:35 +0200

    extracted uWsgi class

---
+++ src/Format/UWsgiFormat.php
@@ -0,0 +1,16 @@
+<?php
+namespace StevieRay\Format;
+
+class UWsgiFormat extends AbstractFormat
+{
+    public function getHeader($projectUrl, $date)
+    {
+        // TODO: Implement getHeader() method.
+    }
+
+    public function getFooter()
+    {
+        // TODO: Implement getFooter() method.
+    }
+
+}
